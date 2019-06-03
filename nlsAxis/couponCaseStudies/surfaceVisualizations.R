d195c435d653d7567ea2c0d1a7fe88691df65222
danibarna <dbarna@mymail.mines.edu>
2019-06-03 11:35:32 -0600

    move surfaceVisualizations.R to case studies

---
+++ nlsAxis/couponCaseStudies/surfaceVisualizations.R
@@ -0,0 +1,63 @@
+
+
+library(rgl)
+library(conicfit)
+library(fields)
+library(useful)
+
+
+newCoupon <- readRDS("newCoupon_9.rds")
+oldCoupon <- readRDS("cropped9.rds")
+nlsCoeff <- readRDS("nlsCoeff.rds")
+
+
+ 
+## calculate points on ideal coupon surface
+circXY <- calculateCircle(nlsCoeff["axisVectorX"],
+                nlsCoeff["axisVectorY"], 1000, steps = 100)
+
+circZ <- seq(min(newCoupon[,3]), max(newCoupon[,3]), length.out = 200)
+
+xyzcoord <- cbind(rep(circXY[,1], 200), rep(circXY[,2],200), rep(circZ, each = 100))
+
+idealCyl <- cylinder3d(center = xyzcoord, radius = 2, closed = TRUE)
+
+open3d()
+par3d(cex=0.7)
+plot3d(oldCoupon[,1],
+       oldCoupon[,2],
+       oldCoupon[,3],
+       type = "s", size = 0.45,
+       xlab = " ", ylab = " ", zlab = " ")
+
+open3d()
+par3d(cex=0.7)
+plot3d(newCoupon[,1],
+       newCoupon[,2],
+       newCoupon[,3],
+       type = "s", size = 0.45,
+       zlab = "z", xlab = "x", ylab = "y")
+lines3d(nlsCoeff["axisVectorX"],
+        nlsCoeff["axisVectorY"],
+        circZ, col = "darkorange",
+        lwd = 3)
+rgl.material(alpha = 0.5, lit = FALSE)
+shade3d(idealCyl, col = "cornflowerblue")
+
+
+
+## --------------------------------------------------
+## unzipped coupon surface
+
+polar <- cart2pol(newCoupon[,1], newCoupon[,2])
+
+plot(polar$theta, newCoupon[,3], 
+     col = color.scale(polar$r, zlim = c(1,1172)), pch = 16,
+     main = "surface plot, coupon 9 polar angle 90",
+     xlab = "angle (radians)", ylab = "z axis")
+
+
+# centroidX <- nlsCoeff["centroidX"]
+# centroidY <- nlsCoeff["centroidY"]
+# axisVectorX <- nlsCoeff["axisVectorX"]
+# axisVectorY <- nlsCoeff["axisVectorY"]
