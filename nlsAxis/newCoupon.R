55416cdfa596c4b1875e7001513ae6377c153de3
danibarna <dbarna@mymail.mines.edu>
2019-06-03 10:53:05 -0600

    add a function to return the rotated pore coords
    the function "newCoupon.R" is similar to the radiusAlgin function but
    returns the coordinates of the rotated coupon. Split these into two to
    compartmentalize function purpose. The new rotated coords are useful for
    coupon visualization

--- nlsAxis/newCoupon.R
+++ nlsAxis/newCoupon.R
@@ -1,17 +1,17 @@
 ##
-## function for use in nls to optimize parameters 
-## centroidX, centroidY, axisVectorX, axisVectorY to best
-## find the center axis. 
+## This is just the radiusAligned function but altered sightly
+## to return the coordinates of the "new" rotated coupon instead of the pore
+## radius. For use in the surface plots
 ##
 ## poreCoordinates - a tall matrix containing x, y, and z coordinates of pores
 ## centroidX, centroidY - x and y coordinates of centroid, respectively
 ## axisVectorX, axisVectorY - x and y directions of axis direction vector
 ##
-## returns radius of aligned coupon from center axis
+## returns coordinates of aligned coupon
 ##
 ## ---------------------------------------------------------------------------
 
-radiusAligned <- function(poreCoordinates, centroidX, centroidY, axisVectorX, axisVectorY){
+newCoupon <- function(poreCoordinates, centroidX, centroidY, axisVectorX, axisVectorY){
   
   centroidGuess <- c(centroidX,centroidY,0)
   
@@ -19,7 +19,5 @@ radiusAligned <- function(poreCoordinates, centroidX, centroidY, axisVectorX, ax
   
   newCoupon <- alignCoupon(poreCoordinates, centroidGuess, axisGuess)
   
-  radius <- sqrt(newCoupon[,1]^2+newCoupon[,2]^2)
-  
-  return(radius)
+  return(newCoupon)
 }
