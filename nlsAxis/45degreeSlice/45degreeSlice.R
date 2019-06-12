6ef57fcc9f1377902b1586c8bb3081f5632488e0
danibarna <dbarna@mymail.mines.edu>
2019-06-12 15:54:48 -0600

    update 45degreeSlice to histogram criterion
    Take out the polar and ellipse slice stuff and instead look at the
    histogram created by binning on the new z axis. Compare these histograms
    for a variety of anlges in angleGrid

--- nlsAxis/45degreeSlice/45degreeSlice.R
+++ nlsAxis/45degreeSlice/45degreeSlice.R
@@ -3,12 +3,11 @@ library(pracma)
 library(useful)
 library(rgl)
 
-setwd("C:/Users/barna/Documents/Coupons/nlsAxis/datasets")
-load("radiusIterations.rda") #to get median radius from nls fit
-load("couponCov.rda") #to get covariates
-
-
 fortyFive <- which(couponCov$polarAngle==45)
+
+aboutY <- function(phi) {matrix( c(cos(phi), 0, sin(phi), 0, 1, 0, -sin(phi), 0, cos(phi)), 3, 3)}
+
+aboutZ <- function(theta) {matrix( c(cos(theta), sin(theta), 0, -sin(theta), cos(theta), 0, 0, 0, 1), 3, 3 )}
 
 #look at a few 45 degree coupons
 for(n in fortyFive[1]){
@@ -19,106 +18,51 @@ for(n in fortyFive[1]){
   load("nlsCoupontest.rda")
   
   # plot old coupon
-  open3d()
-  par3d(cex=0.7)
-  plot3d(oldCoupon[,1],
-         oldCoupon[,2],
-         oldCoupon[,3],
-         type = "s", size = 0.45,
-         xlab = " ", ylab = " ", zlab = " ")
+  # open3d()
+  # par3d(cex=0.7)
+  # plot3d(oldCoupon[,1],
+  #        oldCoupon[,2],
+  #        oldCoupon[,3],
+  #        type = "s", size = 0.45,
+  #        xlab = " ", ylab = " ", zlab = " ")
   
   # center the coupon
   centerCoupon <- cbind(scale(newCoupon[,1], center = TRUE, scale = FALSE),
                         scale(newCoupon[,2], center = TRUE, scale = FALSE),
                         newCoupon[,3])
   
-  open3d()
-  par3d(cex=0.7)
-  plot3d(newCoupon[,1],
-         newCoupon[,2],
-         newCoupon[,3],
-         type = "s", size = 0.45,
-         xlab = " ", ylab = " ", zlab = " ")
-  
-  open3d()
-  par3d(cex=0.7)
-  plot3d(centerCoupon[,1],
-         centerCoupon[,2],
-         centerCoupon[,3],
-         type = "s", size = 0.45,
-         xlab = " ", ylab = " ", zlab = " ")
-  
-  theta <- pi #the angle that we iterate over
   
   angleSeq <- seq(0,pi, by = pi/30) #test out a sequence of theta
   
   for(theta in angleSeq){
     
-    Rxy <- matrix( c(cos(theta), sin(theta), 0, -sin(theta), cos(theta), 0, 0, 0, 1), 3, 3 )
+    Rz <- aboutZ(theta)
     
-    RxyCoupon <- centerCoupon %*% Rxy
+    RzCoupon <- centerCoupon %*% Rz
     
     
     phi <- 45*pi/180 #45 degree tilt
     
     Ry <- aboutY(phi)
     
-    RzCoupon <- RxyCoupon %*% Ry
+    RzyCoupon <- RzCoupon %*% Ry
     
-    
-    #get new center axis for tilted coupon
-    axisMat <- cbind(rep(0,length(centerCoupon[,1])), rep(0,length(centerCoupon[,1])), centerCoupon[,3])
-    
-    newAxis <- axisMat %*% Rz
     
     # open3d()
     # par3d(cex=0.7)
-    # plot3d(RzCoupon[,1],
-    #        RzCoupon[,2],
-    #        RzCoupon[,3],
+    # plot3d(RzyCoupon[,1],
+    #        RzyCoupon[,2],
+    #        RzyCoupon[,3],
     #        type = "s", size = 0.45,
     #        xlab = "x", ylab = "y", zlab = "z")
-    # points3d(newAxis[,1],
-    #          newAxis[,2],
-    #          newAxis[,3],
-    #          size = 3, col = "tomato")
 
-
-    
-    # find angle to origin (newAxis) and horizontal distance
-    
-    centerForPolar <- RzCoupon[,1:2]-newAxis[,1:2]
-    
-    polar <- useful::cart2pol(centerForPolar[,1], centerForPolar[,2])
-    
-    
-    #plot(polar$theta, polar$r, ylim = c(0,1200), main = paste0(n))
-    
-    medianWithoutNA<-function(x) {
-      median(x[which(!is.na(x))])
-    }
-    
-    b = medianWithoutNA(radiusLastIter) #instead of 1000 use median radius from algined coupon
-    a = medianWithoutNA(radiusLastIter)*sec(phi)
-    
-    ellipseR <- function(theta){a*b/sqrt( a^2*sin(theta)^2 + b^2*cos(theta)^2)}
-    
-    #points(polar$theta, ellipseR(polar$theta), col = "red")
-    
-    
-    h<-hist(trunc(RzCoupon[,3]), breaks = 130, col = "darkgrey")
+  
+    h<-hist(trunc(RzyCoupon[,3]), breaks = 130, col = "darkgrey")
     
   }
   
 }
 
 
-open3d()
-par3d(cex=0.7)
-plot3d(RzCoupon[,1],
-       RzCoupon[,2],
-       RzCoupon[,3],
-       type = "s", size = 0.45,
-       xlab = "x", ylab = "y", zlab = "z")
 
 
