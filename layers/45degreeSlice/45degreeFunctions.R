301fa1ff87228bcf186ea9f8f43d3341a18a6787
danibarna <dbarna@mymail.mines.edu>
2019-06-25 11:17:19 -0600

    functions for rotation and periodogram calcs
    takes functions and for loops from earlier versions of 45degreeSlice.R
    and compartmentalizes them here. Useful to analyze many simulated
    coupons. However removed plotting capability

---
+++ layers/45degreeSlice/45degreeFunctions.R
@@ -0,0 +1,93 @@
+## 6/25/2019
+##
+##
+##
+## functionalize 45degreeSlice
+##
+## this function rotates the coupon to find proper rotational
+## orientation. 
+## ------------------------------------------------------
+
+
+## --------------rotation matrices------------------------
+aboutY <- function(phi) {matrix( c(cos(phi), 0, sin(phi), 0, 1, 0, -sin(phi), 0, cos(phi)), 3, 3)}
+
+aboutZ <- function(theta) {matrix( c(cos(theta), sin(theta), 0, -sin(theta), cos(theta), 0, 0, 0, 1), 3, 3 )}
+## ------------------------------------------------------
+
+
+## -------------rotation function------------------------
+FFrotate <- function(angleSeq, centerCoupon){
+
+histSave <- list()
+
+i = 1
+
+for(theta in angleSeq){
+  
+  Rz <- aboutZ(theta)
+  
+  RzCoupon <- centerCoupon %*% Rz
+  
+  
+  phi <- 45*pi/180 #45 degree tilt
+  
+  Ry <- aboutY(phi)
+  
+  RzyCoupon <- RzCoupon %*% Ry
+ 
+  h<-hist(~RzyCoupon[,3], w=25/10, plot = FALSE)
+  
+  histSave[[i]] <- rbind(h$counts, h$mids)
+  
+  i = i + 1
+  
+}
+
+return(histSave)
+
+}
+## ------------------------------------------------------
+
+
+## -----------get relative peak strengths----------------
+makePeriodogram <- function(angleSeq, histSave){
+  
+  findTheta <- rep(NA,length(angleSeq))
+  frDt <- list()
+  Dtlen <- rep(NA, length(angleSeq))
+  
+  relSignal <- rep(NA, length(angleSeq))
+  
+  threshold <- 10
+  influence <- 0
+  
+  for(m in 1:length(angleSeq)){
+    
+    xGrid <- seq(min(histSave[[m]][2,]), max(histSave[[m]][2,]), length.out = length(histSave[[m]][1,]))
+    
+    lowpass <- splint(histSave[[m]][2,], histSave[[m]][1,], xGrid, lambda = 50)
+    
+    frDt[[m]] <- fft(histSave[[m]][1,]-lowpass)
+    
+    Dtlen[m] <- length(histSave[[1]][1,])
+    
+    Fr <- (1:Dtlen[m]/Dtlen[m])[1:(Dtlen[m]/2)]
+    P <- (Mod(2*frDt[[m]]/Dtlen[m])^2)[1:(Dtlen[m]/2)]
+    
+    lag <- length(Fr)/10
+    
+    peak <- findFreq(P,lag,threshold,influence)
+    
+    relSignal[m] <- sum(P[which(peak$signals==1)])/sum(P)
+    
+  }
+  
+  return(relSignal)
+  
+}
+## ------------------------------------------------------
+
+
+
+
