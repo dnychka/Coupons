0732bc06989d278870e5adf8aa05a449ec5fa0b1
danibarna <dbarna@mymail.mines.edu>
2019-07-15 10:42:57 -0600

    update file to current syntax

--- layers/0degreeSlice/testRealCoupons0.R
+++ layers/0degreeSlice/testRealCoupons0.R
@@ -10,35 +10,34 @@ library(rgl)
 library(fields)
 library(FSA)
 
-setwd("C:/Users/barna/Documents/Coupons/layers/45degreeSlice")
-source("threshold.R")
-source("45degreeFunctions.R")
+setwd("C:/Users/barna/Documents/Coupons/layers/0degreeSlice")
+source("findPeaks.R")
 
-setwd("C:/Users/barna/Documents/Coupons/nlsAxis/datasets")
+setwd("C:/Users/barna/Documents/Coupons/datasets")
 load("couponCov.rda")
 
-setwd("C:/Users/barna/Documents/Coupons/layers/0degreeSlice/0degreeData/nullHyp")
-harmonicSignalsNull <- readRDS("harmonicSignalsNull.rds")
-
-setwd("C:/Users/barna/Documents/Coupons/nlsAxis/couponCaseStudies/caseStudyData/cropped")
+setwd("C:/Users/barna/Documents/Coupons/nlsAxis/porosity/nlsPorosityData/cropped")
 
 ## ------------------------------------------------------
 
 Zero <- which(couponCov$polarAngle == 0)
 
 numCoupons <- length(Zero)
-realSignals <- vector()
+realSignals <- rep(NA, ncol = length(Zero))
 
+k=1
 
 for(n in Zero){
   
   print(n)
   
   load(paste0("nlsCoupon", n, ".rda"))
+  
   # center the coupon
   centerCoupon <- cbind(scale(newCoupon[,1], center = TRUE, scale = FALSE),
                         scale(newCoupon[,2], center = TRUE, scale = FALSE),
                         newCoupon[,3])
+  
   
   h<-hist(~newCoupon[,3], w=5, plot = FALSE)
   
@@ -59,7 +58,7 @@ for(n in Zero){
   Fr <- (1:Dtlen/Dtlen)[1:(Dtlen/2)]
   P <- (Mod(2*frDt/Dtlen)^2)[1:(Dtlen/2)]
   
-  plot(Fr,P,type="l", main = paste0(n))
+  plot(Fr,P,type="l", main = paste0("layer spacing at ", round(n,3)))
   
   lag <- length(Fr)/10
   
@@ -67,71 +66,51 @@ for(n in Zero){
   
   ind <- which(peak$signals==1)
   
-  Ppeaks <- P[ind]
-  Frpeaks <- Fr[ind]
   
-  # fundFreq <- Frpeaks[which.max(Ppeaks)]
-  # findHarmonic <- vector()
-  # 
-  # 
-  # ##check for lowest freq (i.e. fundamental frequency)
-  # 
-  # for(j in c(1/4,1/3,1/2,2,3,4,5)){
-  #   for(i in 1:length(Frpeaks)){
-  #     ifelse(all.equal(fundFreq*j, Frpeaks[i], tol = 0.01)==TRUE,  fundFreq <- c(fundFreq,Frpeaks[i]),  NA)
-  #   }
-  # }
-  # 
-  # xline(fundFreq, col = "grey", lty=3)
-  # 
-  # fundFreq <- min(fundFreq)
-  # 
-  # xline(fundFreq, col = "violetred1", lty=3)
-  # 
-  # ##find the harmonics
-  # for(j in 1:5){
-  #   for(i in 1:length(Fr)){
-  #     ifelse(all.equal(fundFreq*j, Fr[i], tol = 0.01)==TRUE,  findHarmonic <- c(findHarmonic, P[i]),  NA)
-  #   }
-  # }
+  
   
   findHarmonic <- vector()
+  findBand <- vector()
   
-  origFreq <- Frpeaks[which.max(Ppeaks)]
-  
-  for(j in c(1,1/2,1/3,1/4,1/5)){
-    fundFreq <- origFreq*j
-    if(fundFreq <= .15){break}
-  }
-  
-  xline(fundFreq*c(1,2,3,4,5), col = "tomato", lty=3)
-  
-  ##find the harmonics
-  for(j in 1:5){
-    for(i in 1:length(Fr)){
-      ifelse(all.equal(fundFreq*j, Fr[i], tol = 0.05)==TRUE, findHarmonic <- c(findHarmonic, P[i]),  NA)
-      
+  if(length(ind) != 0){# check to make sure there's actually peaks
+    
+    Ppeaks <- P[ind]
+    Frpeaks <- Fr[ind]
+    
+    origFreq <- Frpeaks[which.max(Ppeaks)]
+    
+    for(j in c(1,1/2,1/3,1/4,1/5)){
+      fundFreq <- origFreq*j
+      if(fundFreq <= .15){break}
     }
+    ##find the harmonics
+    for(j in 1:5){
+      for(i in 1:length(Fr)){
+        ifelse(isTRUE(all.equal(fundFreq*j, Fr[i], tol = 0.05)), findHarmonic <- c(findHarmonic, P[i]),  NA)
+        ifelse(isTRUE(all.equal(fundFreq*j, Fr[i], tol = 0.05)), findBand <- c(findBand, Fr[i]),  NA)
+      }
+    }
+    
+    xline(findBand, col = "violetred1", lty=3)
+    
+  } else {
+    fundFreq <- Fr[which.max(P[which(Fr<=0.15)])]
+    ##find the harmonics
+    for(j in 1:5){
+      for(i in 1:length(Fr)){
+        ifelse(isTRUE(all.equal(fundFreq*j, Fr[i], tol = 0.05)), findHarmonic <- c(findHarmonic, P[i]),  NA)
+        ifelse(isTRUE(all.equal(fundFreq*j, Fr[i], tol = 0.05)), findBand <- c(findBand, Fr[i]),  NA)
+      }
+    }
+    xline(findBand, col = "violetred1", lty=3)
   }
   
+  
+  realSignals[k] <- sum(findHarmonic)/sum(P)
 
-realSignals <- c(realSignals, sum(findHarmonic)/sum(P))
+ k=k+1
 
 }
-layeredCoupons <- harmonicSignals
-nullCoupons <- c(harmonicSignalsNull, rep(NA, length(layeredCoupons)-length(harmonicSignalsNull)))
-realCoupons <- c(realSignals, rep(NA, (length(nullCoupons)-length(realSignals))))
 
-dat <- cbind(nullCoupons, realCoupons)
 
-#dev.off()
-boxplot(dat, main = "relative peak strength, 0 degree coupons",
-        pch = 16, boxwex = 0.7)
-
-dat <- cbind(dat, layeredCoupons)
-
-boxplot(dat, main = "relative peak strength, 0 degree coupons",
-        pch = 16, boxwex = 0.7)
-
-t.test(nullCoupons, realCoupons)
-t.test(harmonicSignals, realCoupons)
+saveRDS(realSignals, "zerosRealSignals.rds")
