26004a41b161674ed5a8a07bc0e3d88077d0a835
danibarna <dbarna@mymail.mines.edu>
2020-02-03 13:08:43 -0700

    bring 45deg sim into the modern age
    calls nlsFucntions with five parameter optimization, internal jitter so
    nls doesn't choke, runs 10,000 simulations in the blink of an eye!

--- layers/simulateCoupons/makeCoupon45.R
+++ layers/simulateCoupons/makeCoupon45.R
@@ -10,14 +10,14 @@ library(conicfit)
 library(rgl)
 
 
+setwd("C:/Users/barna/Documents/Coupons/nlsAxis")
+source("nlsFunctions.R")
 
-numLayer <- seq(40,60,length.out = 100)
 
-noiseAmt <- 0.5
+numLayer <- seq(40,60,length.out = 10000)
 
-numLayerFine <- seq(10,30,length.out = 50)
+rnoise <- 50
 
-which(round(numLayer,3)==53.061)
 
 for(l in numLayer){
 # calc points on a 45 degree line
@@ -25,8 +25,8 @@ x = seq(0,4000,by = l)
 y = rep(0, length(x))
 
 z=x
-# noise <- runif(length(x),-l*noiseAmt,l*noiseAmt)
-# z = x + noise
+#noise <- runif(length(x),-l*noiseAmt,l*noiseAmt)
+#z = x + noise
 
 
 phi = 45*pi/180
@@ -34,7 +34,8 @@ a = 814*sec(phi) #major axis
 b = 814 #minor axis
 
 pts <- matrix(NA, nrow = 25, ncol = 3)
-pts[,1:2] <- calculateEllipse(x[1],y[1],a,b, steps = 25, randomDist = TRUE)
+pts[,1:2] <- calculateEllipse(x[1],y[1],a,a, steps = 25, noiseFun=function(x) 
+  (x+runif(1,-rnoise,rnoise)))
 pts[,3] <- rep(z[1], 25)
 
 ellipseStack <- pts
@@ -60,96 +61,27 @@ Ry <- aboutY(theta)
 
 turnedStack <- ellipseStack %*% Ry 
 
-## ----------------------------------------------------------------------
-## modified version of nlsCouponFit.R
-## removed while loop--since the radius of the ellipse is set,
-## nls finds the optimal orientation quite fast and there's zero
-## distance between the ellipse points and the target radius,
-## casuing the step size to be reduced past minimum threshold
-## ----------------------------------------------------------------------
 
-deciles <- quantile(turnedStack[,3], prob = seq(0, 1, length = 11), type = 5)
-
-comX <- turnedStack[,1]
-comY <- turnedStack[,2]
-comZ <- turnedStack[,3]
-
-## subset the coupon to avoid the weld/support material remnants
-
-good <- (comZ >= deciles[3] & comZ <= deciles[9])
-
-poreCoordinates <- cbind( comX,
-                          comY,
-                          comZ)
-
-oldCoupon <- poreCoordinates # to save the orginial coords for 3d plotting
-
-## get centers of mass for upper and lower half of the coupon to 
-## compute axisVector, which is the direction vector for 
-## the initial axis estimate
-upperHalf <- (comZ >= deciles[6]) & good 
-
-lowerHalf <- (comZ <= deciles[6]) & good
-
-axisVector <- colMeans(poreCoordinates[upperHalf,]) - colMeans(poreCoordinates[lowerHalf,])
-
-axisVector <- axisVector / sqrt(sum(axisVector^2)) # make it unit length
-
-
-poreCoordinates <- cbind( comX[good],
-                          comY[good],
-                          comZ[good])
-
-
-## project the centroid into the x-y plane along axisVector.
-## allows us the parameterize the centroid using two parameters
-## (x,y,0) rather than three (x,y,z)
-centroid <- colMeans(poreCoordinates)
-
-xyCentroid <- c( (axisVector[1]*-centroid[3])/axisVector[3] + centroid[1],
-                 (axisVector[2]*-centroid[3])/axisVector[3] + centroid[2],
-                 0)
-
-
-## initial radius guess
-r = 1000 # based on ideal coupon radius of 1000 micor-meters
-
-setwd("C:/Users/barna/Documents/Coupons/nlsAxis")
-source("rotateCoupon.R")
-source("getRadius.R")
-
-
-N <- length(poreCoordinates[,1])
-
-radiusTarget <- rep(r, N) 
-
-
-
-centerAxis <- NULL
-attempt <- 1
-while( is.null(centerAxis) && attempt <= 10 ) {
+while(class(nlsObj) == "try-error"){ 
   print(l)
-  attempt <- attempt + 1
-  try(centerAxis <- nls(radiusTarget~radiusAligned(poreCoordinates, centroidX, centroidY, axisVectorX, axisVectorY),
-                        start = list(centroidX = xyCentroid[1], centroidY = xyCentroid[2],
-                                     axisVectorX = axisVector[1], axisVectorY = axisVector[2]),
-                        control = nls.control(minFactor = 1/10000000000000)))
+  nlsObj <- try(nlsAxisFit(turnedStack))
 }
-nlsCoeff <- coef(centerAxis)
 
+nlsCoeff <- coef(nlsObj)
 
-  
-  
-
+setwd("C:/Users/barna/Documents/Coupons/layers/layerData/45degreeData/spacing40to60")
 
 ## store the old coupon coordinates, the "new" rotated coupon coords, and the nls coeff
 ## useful for generating surface plots and histograms for each coupon
-source("newCoupon.R")
-setwd("C:/Users/barna/Documents/Coupons/layers/layerData/45degreeData/spacing40to60")
-nlsCoupon <- newCoupon(poreCoordinates, nlsCoeff["centroidX"], nlsCoeff["centroidY"], 
+nlsCoupon <- newCoupon(turnedStack, nlsCoeff["centroidX"], nlsCoeff["centroidY"], 
                        nlsCoeff["axisVectorX"], nlsCoeff["axisVectorY"])
 
+oldCoupon <- turnedStack
+
+print(round(l,3))
+
 save(oldCoupon, nlsCoupon, nlsCoeff, file = paste0(round(l,3),"spacingSyn45.rda"))
+
 }
   
 
